780c7d0ecb09f4074fb527e6e00681cc80f1b10f
boyfromthewell <tnsdyd10@naver.com>
2023-08-02 18:52:53 +0900

    feat: get stores information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from 'react';
+import { GetStaticProps } from 'next';
 import Header from '@/components/common/Header';
 import MapSection from '@/components/home/MapSection';
 import styles from '@/styles/header.module.scss';
 import Link from 'next/link';
 import { AiOutlineShareAlt } from 'react-icons/ai';
 import { VscFeedback } from 'react-icons/vsc';
+import { Store } from '../../types/store';
+import useStores from '@/hooks/useStores';
 
-const Home = () => {
+interface HomeProps {
+  stores: Store[];
+}
+
+const Home = ({ stores }: HomeProps) => {
+  const { initializeStores } = useStores();
+
+  useEffect(() => {
+    // 전역 상태 업데이트
+    initializeStores(stores);
+  }, [initializeStores, stores]);
+
   return (
     <>
       <Header
@@ -31,3 +46,12 @@ const Home = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const stores = (await import('../../public/stores.json')).default;
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
+};
