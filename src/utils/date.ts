d1e749a53b26c50bcd6605b1cfd7d93753472bf2
boyfromthewell <tnsdyd10@naver.com>
2023-08-09 15:58:53 +0900

    fix: date parsing function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,8 +6,8 @@ export const dateParsing = (dateNum: number) => {
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
 
-  const hour = date.getHours();
-  const min = date.getMinutes();
+  const hour = String(date.getHours()).padStart(2, '0');
+  const min = String(date.getMinutes()).padStart(2, '0');
 
   return `${year}/${month}/${day} ${hour}:${min}`;
 };
